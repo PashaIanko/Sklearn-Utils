53a3c8e305929a0da6c5b67d2834e7dc8f92f1e4
Pavel Ianko <55174155+PashaIanko@users.noreply.github.com>
2022-11-01 16:07:44 +0100

    outlier cleaner class

--- sklearn_transformers.py
+++ sklearn_transformers.py
@@ -31,3 +31,32 @@ class LogTransformer(BaseEstimator, TransformerMixin):
             assert c in transformed.columns
             transformed[c] = np.log(transformed[c] + self.offset)
         return transformed
+
+
+class OutlierCleaner(BaseEstimator, TransformerMixin):
+    def __init__(self, columns_, lower_quantile_, upper_quantile_):
+        super().__init__()
+        self.columns = columns_
+        self.lower_quantile = lower_quantile_
+        self.upper_quantile = upper_quantile_
+        self.quantiles = {}
+
+    def fit(self, X, y=None):
+        for column in self.columns:
+            self.quantiles[column] = {
+                'lower': X[column].quantile(self.lower_quantile),
+                'upper': X[column].quantile(self.upper_quantile)
+            }
+        return self
+
+    def transform(self, X, y=None):
+        transformed_df = X.copy()
+
+        for column in self.columns:
+            transformed_df[column] = \
+                transformed_df[column].clip(
+                    lower=self.quantiles[column]['lower'],
+                    upper=self.quantiles[column]['upper']
+                )
+        del X
+        return transformed_df
